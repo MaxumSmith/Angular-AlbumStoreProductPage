56a9ff4d912d90707a2fbb9f4d75b8f32b73e7ca
Maxum Smith <Maxum.Smith@healthcarousel.com>
2019-05-18 08:26:15 -0400

    ci
    ci

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,9 +11,10 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(_productService: ProductService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe(response => this.products = response);
   }
 
 }
